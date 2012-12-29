cec6195f408a4d4febf253c62001a7f4b8f08c47
Clément BADIOLA <cidhair@free.fr>
2012-12-29 17:16:49 +0100

    Hubs and authorities adapté. Mélange des version networkX et algorithme original. Testé sur un graphe généré à partir de 3 boites mails enron.

--- src/enronImport.py
+++ src/enronImport.py
@@ -4,8 +4,8 @@ from email.utils import parsedate_tz
 from email.utils import mktime_tz
 from datetime import datetime
 from datetime import timedelta
-from moduleclique import find_cliques
 import os
+import math
 
 class mailParser():
 	def __init__(self, graph, nodeName, filepath):
@@ -25,45 +25,45 @@ class mailParser():
 		# For each mail sent
 		for file in listing_sent:
 			sent_mails = sent_mails + 1
-   			currentMail = open(pathToParse+file, 'rb')
-   			msg = Parser().parse(currentMail)
-   			if type(msg['To']) is str:
-   				recipients = msg['To'].split(',')
-   				date = self.extractDateFromMsg(msg)
-   				for to in recipients:
-   					self.users[person]["sent_list"].append([to.strip(),date])
-   			if type(msg['From']) is str:
-   				expeditor = msg['From']
-   				expeditors.append(expeditor)
-   			self.register(expeditor, recipients)
-   			currentMail.close()
-   		self.users[person]["sent_mails"] = sent_mails
-   		self.personWithMultipleAdress(list(set(expeditors)))
-   		
-   	def parse_received(self, person):
-   	   pathToParse = self.f + person + "/inbox/"
-   	   listing_received = os.listdir(pathToParse)
-   	   received_mails = 0
-   	   delta_cpt = 0
-   	   for file in listing_received:
-   			if os.path.isfile(pathToParse+file):
-   				received_mails = received_mails + 1
-	   			currentMail = open(pathToParse+file, 'rb')
-	   			msg = Parser().parse(currentMail)
-	   			if type(msg['From']) is str:
-	   				expeditor = msg['From'].strip()
-	   				date = self.extractDateFromMsg(msg)
-	   				for pair in self.users[person]["sent_list"]:
-	   					if expeditor == pair[0]:
-	   						if date > pair[1]:
-	   							delta = date - pair[1]
-	   							if delta <= self.d:
-	   								delta_cpt = delta_cpt + 1
-	   								self.users[person]["response_time"] = self.users[person]["response_time"] + delta.total_seconds()
-	   								break
-	   self.users[person]["received_mails"] = received_mails
-	   self.users[person]["response_time"] = self.users[person]["response_time"] / delta_cpt
-   		#print(received_mails)
+			currentMail = open(pathToParse+file, 'rb')
+			msg = Parser().parse(currentMail)
+			if type(msg['To']) is str:
+				recipients = msg['To'].split(',')
+				date = self.extractDateFromMsg(msg)
+				for to in recipients:
+					self.users[person]["sent_list"].append([to.strip(),date])
+			if type(msg['From']) is str:
+				expeditor = msg['From']
+				expeditors.append(expeditor)
+			self.register(expeditor, recipients)
+			currentMail.close()
+		self.users[person]["sent_mails"] = sent_mails
+		self.personWithMultipleAdress(list(set(expeditors)))
+		
+	def parse_received(self, person):
+		pathToParse = self.f + person + "/inbox/"
+		listing_received = os.listdir(pathToParse)
+		received_mails = 0
+		delta_cpt = 0
+		for file in listing_received:
+			if os.path.isfile(pathToParse+file):
+				received_mails = received_mails + 1
+				currentMail = open(pathToParse+file, 'rb')
+				msg = Parser().parse(currentMail)
+				if type(msg['From']) is str:
+					expeditor = msg['From'].strip()
+					date = self.extractDateFromMsg(msg)
+					for pair in self.users[person]["sent_list"]:
+						if expeditor == pair[0]:
+							if date > pair[1]:
+								delta = date - pair[1]
+								if delta <= self.d:
+									delta_cpt = delta_cpt + 1
+									self.users[person]["response_time"] = self.users[person]["response_time"] + delta.total_seconds()
+									break
+		self.users[person]["received_mails"] = received_mails
+		self.users[person]["response_time"] = self.users[person]["response_time"] / delta_cpt
+		#print(received_mails)
 	
 	def extractDateFromMsg(self, msg):
 		date = None
@@ -82,8 +82,8 @@ class mailParser():
 			self.users[person]["response_time"] = 0.0
 			self.parse_sent(person)
 			self.parse_received(person)
-	   #	print self.users
-   	
+		#    print self.users
+	
 	def personWithMultipleAdress(self, otherAdresses):
 		first = otherAdresses.pop()
 		for other in otherAdresses:
@@ -94,141 +94,186 @@ class mailParser():
 					firstNode = node
 			edge = self.g.addEdge(firstNode, otherNode)
 			self.color[edge] = tlp.Color(255, 0, 0)
-   	
-   	def register(self, expeditor, recipients):
-   		#Traitement de l'expediteur
-   		flag = False
-   		for nodes in self.g.getNodes():
-   			if expeditor in self.names[nodes]:
-   				flag = True
-   				expeditorNode = nodes
-   				break
-   		if not flag:
-   			node = self.g.addNode()
-   			expeditorNode = node
-   			self.names[node] = expeditor.strip()
-   		#Traitement des recepteurs
-   		for adress in recipients:
-   			adress = adress.strip()
-   			flag = False
-   			for node in self.g.getNodes():
-   				if adress in self.names[node]:
-   					flag = True
-   					receptorNode = node
-   					break
-   			if not flag:
-   				node = self.g.addNode()
-   				receptorNode = node
-   				self.names[node] = adress
-   			self.g.addEdge(expeditorNode, receptorNode)
+	
+	def register(self, expeditor, recipients):
+		#Traitement de l'expediteur
+		flag = False
+		for nodes in self.g.getNodes():
+			if expeditor in self.names[nodes]:
+				flag = True
+				expeditorNode = nodes
+				break
+		if not flag:
+			node = self.g.addNode()
+			expeditorNode = node
+			self.names[node] = expeditor.strip()
+		#Traitement des recepteurs
+		for adress in recipients:
+			adress = adress.strip()
+			flag = False
+			for node in self.g.getNodes():
+				if adress in self.names[node]:
+					flag = True
+					receptorNode = node
+					break
+			if not flag:
+				node = self.g.addNode()
+				receptorNode = node
+				self.names[node] = adress
+			self.g.addEdge(expeditorNode, receptorNode)
+
+
+#End of MailParse class
 
 def find_cliques(graph):
-	#Cache nbrs and find first pivot (highest degree)
-	maxconn=-1
-	nnbrs={}
-	pivotnbrs=set() # handle empty graph
-	for n in graph.getNodes():
-		nbrs=set(graph.getInOutNodes(n))
-		nbrs.discard(n)
-		conn = len(nbrs)
-		if conn > maxconn:
-			nnbrs[n] = pivotnbrs = nbrs		
-			maxconn = conn	
-		else :
-			nnbrs[n] = nbrs 
+    #Cache nbrs and find first pivot (highest degree)
+    maxconn=-1
+    nnbrs={}
+    pivotnbrs=set() # handle empty graph
+    for n in graph.getNodes():
+        nbrs=set(graph.getInOutNodes(n))
+        nbrs.discard(n)
+        conn = len(nbrs)
+        if conn > maxconn:
+            nnbrs[n] = pivotnbrs = nbrs     
+            maxconn = conn  
+        else :
+            nnbrs[n] = nbrs 
 
-	# Initial setup
-	cand=set(nnbrs)
-	smallcand = set(cand - pivotnbrs) 
-	done=set()
-	stack=[]
-	clique_so_far=[]
-	# Start main loop
-	while smallcand or stack:
-		try:
-			# Any nodes left to check?
-			n=smallcand.pop()
-		except KeyError:
-			# back out clique_so_far
-			cand,done,smallcand = stack.pop()
-			clique_so_far.pop()
-			continue
+    # Initial setup
+    cand=set(nnbrs)
+    smallcand = set(cand - pivotnbrs) 
+    done=set()
+    stack=[]
+    clique_so_far=[]
+    # Start main loop
+    while smallcand or stack:
+        try:
+            # Any nodes left to check?
+            n=smallcand.pop()
+        except KeyError:
+            # back out clique_so_far
+            cand,done,smallcand = stack.pop()
+            clique_so_far.pop()
+            continue
           
-		# Add next node to clique
-		clique_so_far.append(n)
-		cand.remove(n) #on supprimer le noeud pivot de la liste
-		done.add(n)
-		nn=nnbrs[n] # on recupere les voisins du noeuds
-		new_cand = cand & nn
-		new_done = done & nn 
+        # Add next node to clique
+        clique_so_far.append(n)
+        cand.remove(n) #on supprimer le noeud pivot de la liste
+        done.add(n)
+        nn=nnbrs[n] # on recupere les voisins du noeuds
+        new_cand = cand & nn
+        new_done = done & nn 
 
-		# check if we have more to search
-		if not new_cand: 
-			if not new_done:
-				# Found a clique!
-				yield clique_so_far[:]
-			clique_so_far.pop()
-			continue
+        # check if we have more to search
+        if not new_cand: 
+            if not new_done:
+                # Found a clique!
+                yield clique_so_far[:]
+            clique_so_far.pop()
+            continue
 
-		# Shortcut--only one node left!
-		if not new_done and len(new_cand)==1:
-			yield clique_so_far + list(new_cand)
-			clique_so_far.pop()
-			continue
+        # Shortcut--only one node left!
+        if not new_done and len(new_cand)==1:
+            yield clique_so_far + list(new_cand)
+            clique_so_far.pop()
+            continue
 
-		# find pivot node (max connected in cand)
-		# look in done nodes first
-		numb_cand=len(new_cand)
-		maxconndone=-1
-		for n in new_done:
-			cn = new_cand & nnbrs[n]
-			conn=len(cn)
-			if conn > maxconndone:
-				pivotdonenbrs=cn
-				maxconndone=conn
-				if maxconndone==numb_cand:
-					break
-			
-		# Shortcut--this part of tree already searched
-		if maxconndone == numb_cand:  
-			clique_so_far.pop()
-			continue
-			
-		# still finding pivot node
-		# look in cand nodes second
-		maxconn=-1
-		for n in new_cand:
-			cn = new_cand & nnbrs[n]
-			conn=len(cn)
-			if conn > maxconn:
-				pivotnbrs=cn
-				maxconn=conn
-				if maxconn == numb_cand-1:
-					break 
+        # find pivot node (max connected in cand)
+        # look in done nodes first
+        numb_cand=len(new_cand)
+        maxconndone=-1
+        for n in new_done:
+            cn = new_cand & nnbrs[n]
+            conn=len(cn)
+            if conn > maxconndone:
+                pivotdonenbrs=cn
+                maxconndone=conn
+                if maxconndone==numb_cand:
+                    break
+            
+        # Shortcut--this part of tree already searched
+        if maxconndone == numb_cand:  
+            clique_so_far.pop()
+            continue
+            
+        # still finding pivot node
+        # look in cand nodes second
+        maxconn=-1
+        for n in new_cand:
+            cn = new_cand & nnbrs[n]
+            conn=len(cn)
+            if conn > maxconn:
+                pivotnbrs=cn
+                maxconn=conn
+                if maxconn == numb_cand-1:
+                    break 
 
-		# pivot node is max connected in cand from done or cand
-		if maxconndone > maxconn:
-			pivotnbrs = pivotdonenbrs
-		# save search status for later backout
-		stack.append( (cand, done, smallcand) )
-		cand=new_cand
-		done=new_done
-		smallcand = cand - pivotnbrs
+        # pivot node is max connected in cand from done or cand
+        if maxconndone > maxconn:
+            pivotnbrs = pivotdonenbrs
+        # save search status for later backout
+        stack.append( (cand, done, smallcand) )
+        cand=new_cand
+        done=new_done
+        smallcand = cand - pivotnbrs
 
 def compute_cliqueNumber(liste, node):
-	cpt = 0
-	for clique in liste :
-		if node in clique:
-			cpt = cpt+1
-	return cpt
+    cpt = 0
+    for clique in liste :
+        if node in clique:
+            cpt = cpt+1
+    return cpt
+    
+def hits(graph, auth, hub, max_iter=100, tol=1.0e-8):
+	i = 0
+	for n in graph.getNodes():
+		auth[n] = hub[n] = 1.0 / graph.numberOfNodes()
+	while True:
+		hublast = hub
+		norm = 1.0
+		normsum = 0
+		err = 0
+		for n in graph.getNodes():
+			auth[n] = 0
+			for v in graph.getInNodes(n):
+				auth[n] = auth[n] + hub[v]
+			normsum = normsum + auth[n]
+		norm = norm / normsum
+		for n in graph.getNodes():
+			auth[n] = auth[n] * norm
+		norm = 1.0
+		for n in graph.getNodes():
+			hub[n] = 0
+			for v in graph.getOutNodes(n):
+				hub[n] = hub[n] + auth[v]
+			normsum = normsum + hub[n]
+		norm = norm / normsum
+		for n in graph.getNodes():
+			hub[n] = hub[n] * norm
+		for n in graph.getNodes():
+			err = err + abs(hub[n] - hublast[n])
+		if err < tol:
+			break
+		if i > max_iter:
+			raise HITSError(\
+			"HITS: failed to converge in %d iterations."%(i+1))
+		i = i + 1
+	return auth,hub
+	
 
 def main(graph): 
-	for edge in graph.getEdges():
-		graph.delEdge(edge)
-	for node in graph.getNodes():
-		graph.delNode(node)
-	nodeName =  graph.getStringProperty("nodeName")
-	myParser = mailParser(graph, nodeName, "/net/cremi/cbadiola/travail/bioInfo/enron_mail_20110402/maildirtest/")
-	myParser.parse()
+    for edge in graph.getEdges():
+        graph.delEdge(edge)
+    for node in graph.getNodes():
+        graph.delNode(node)
+    nodeName = graph.getStringProperty("nodeName")
+    #enronpath = "/net/cremi/cbadiola/travail/bioInfo/enron_mail_20110402/maildirtest/"
+    enronpath = "C:/Users/admin/Downloads/enron_mail_20110402/"
+    myParser = mailParser(graph, nodeName, enronpath)
+    myParser.parse()
+    auth = graph.getDoubleProperty("auth")
+    hub = graph.getDoubleProperty("hub")
+    hits(graph, auth, hub)
 
 
