1f2ccbafd44a3acf289462a080a5c064b41b5b58
smdasilva <smdasilva@wanadoo.fr>
2013-01-01 21:09:51 +0100

    version sam finit

--- src/enronImport.py
+++ src/enronImport.py
@@ -13,8 +13,8 @@ class mailParser():
 		self.g = graph
 		self.names = self.g.getStringProperty("nodeName")
 		self.color = self.g.getColorProperty("viewColor")
-		self.receivedMails = self.g.getIntegerProperty("received_mails")
-		self.sentMails = self.g.getIntegerProperty("sent_mails")
+		self.receivedMails = self.g.getDoubleProperty("received_mails")
+		self.sentMails = self.g.getDoubleProperty("sent_mails")
 		self.avgResponseTime = self.g.getDoubleProperty("response_time")
 		self.users = {}
 		self.link = {}
@@ -156,106 +156,6 @@ class mailParser():
 
 
 #End of MailParse class
-
-def find_cliques(graph):
-    #Cache nbrs and find first pivot (highest degree)
-    maxconn=-1
-    nnbrs={}
-    pivotnbrs=set() # handle empty graph
-    for n in graph.getNodes():
-        nbrs=set(graph.getInOutNodes(n))
-        nbrs.discard(n)
-        conn = len(nbrs)
-        if conn > maxconn:
-            nnbrs[n] = pivotnbrs = nbrs     
-            maxconn = conn  
-        else :
-            nnbrs[n] = nbrs 
-
-    # Initial setup
-    cand=set(nnbrs)
-    smallcand = set(cand - pivotnbrs) 
-    done=set()
-    stack=[]
-    clique_so_far=[]
-    # Start main loop
-    while smallcand or stack:
-        try:
-            # Any nodes left to check?
-            n=smallcand.pop()
-        except KeyError:
-            # back out clique_so_far
-            cand,done,smallcand = stack.pop()
-            clique_so_far.pop()
-            continue
-          
-        # Add next node to clique
-        clique_so_far.append(n)
-        cand.remove(n) #on supprimer le noeud pivot de la liste
-        done.add(n)
-        nn=nnbrs[n] # on recupere les voisins du noeuds
-        new_cand = cand & nn
-        new_done = done & nn 
-
-        # check if we have more to search
-        if not new_cand: 
-            if not new_done:
-                # Found a clique!
-                yield clique_so_far[:]
-            clique_so_far.pop()
-            continue
-
-        # Shortcut--only one node left!
-        if not new_done and len(new_cand)==1:
-            yield clique_so_far + list(new_cand)
-            clique_so_far.pop()
-            continue
-
-        # find pivot node (max connected in cand)
-        # look in done nodes first
-        numb_cand=len(new_cand)
-        maxconndone=-1
-        for n in new_done:
-            cn = new_cand & nnbrs[n]
-            conn=len(cn)
-            if conn > maxconndone:
-                pivotdonenbrs=cn
-                maxconndone=conn
-                if maxconndone==numb_cand:
-                    break
-            
-        # Shortcut--this part of tree already searched
-        if maxconndone == numb_cand:  
-            clique_so_far.pop()
-            continue
-            
-        # still finding pivot node
-        # look in cand nodes second
-        maxconn=-1
-        for n in new_cand:
-            cn = new_cand & nnbrs[n]
-            conn=len(cn)
-            if conn > maxconn:
-                pivotnbrs=cn
-                maxconn=conn
-                if maxconn == numb_cand-1:
-                    break 
-
-        # pivot node is max connected in cand from done or cand
-        if maxconndone > maxconn:
-            pivotnbrs = pivotdonenbrs
-        # save search status for later backout
-        stack.append( (cand, done, smallcand) )
-        cand=new_cand
-        done=new_done
-        smallcand = cand - pivotnbrs
-
-def compute_cliqueNumber(liste, node):
-    cpt = 0
-    for clique in liste :
-        if node in clique:
-            cpt = cpt+1
-    return cpt
     
 def hits(graph, auth, hub, max_iter=100, tol=1.0e-8):
 	i = 0
@@ -293,9 +193,150 @@ def hits(graph, auth, hub, max_iter=100, tol=1.0e-8):
 		i = i + 1
 	return auth,hub
 	
-def compute_mailNumber(graph,receivedMails,sentMails,totalMail):
+
+def find_cliques(graph):
+	#Cache nbrs and find first pivot (highest degree)
+	maxconn=-1
+	nnbrs={}
+	pivotnbrs=set() # handle empty graph
 	for n in graph.getNodes():
-		totalMail[n] = receivedMails[n] + sentMails[n]
+		nbrs=set(graph.getInOutNodes(n))
+		nbrs.discard(n)
+		conn = len(nbrs)
+		if conn > maxconn:
+			nnbrs[n] = pivotnbrs = nbrs		
+			maxconn = conn	
+		else :
+			nnbrs[n] = nbrs 
+
+	# Initial setup
+	cand=set(nnbrs)
+	smallcand = set(cand - pivotnbrs) 
+	done=set()
+	stack=[]
+	clique_so_far=[]
+	# Start main loop
+	while smallcand or stack:
+		try:
+			# Any nodes left to check?
+			n=smallcand.pop()
+		except KeyError:
+			# back out clique_so_far
+			cand,done,smallcand = stack.pop()
+			clique_so_far.pop()
+			continue
+          
+		# Add next node to clique
+		clique_so_far.append(n)
+		cand.remove(n) #on supprimer le noeud pivot de la liste
+		done.add(n)
+		nn=nnbrs[n] # on recupere les voisins du noeuds
+		new_cand = cand & nn
+		new_done = done & nn 
+
+		# check if we have more to search
+		if not new_cand: 
+			if not new_done:
+				# Found a clique!
+				yield clique_so_far[:]
+			clique_so_far.pop()
+			continue
+
+		# Shortcut--only one node left!
+		if not new_done and len(new_cand)==1:
+			yield clique_so_far + list(new_cand)
+			clique_so_far.pop()
+			continue
+
+		# find pivot node (max connected in cand)
+		# look in done nodes first
+		numb_cand=len(new_cand)
+		maxconndone=-1
+		for n in new_done:
+			cn = new_cand & nnbrs[n]
+			conn=len(cn)
+			if conn > maxconndone:
+				pivotdonenbrs=cn
+				maxconndone=conn
+				if maxconndone==numb_cand:
+					break
+			
+		# Shortcut--this part of tree already searched
+		if maxconndone == numb_cand:  
+			clique_so_far.pop()
+			continue
+			
+		# still finding pivot node
+		# look in cand nodes second
+		maxconn=-1
+		for n in new_cand:
+			cn = new_cand & nnbrs[n]
+			conn=len(cn)
+			if conn > maxconn:
+				pivotnbrs=cn
+				maxconn=conn
+				if maxconn == numb_cand-1:
+					break 
+
+		# pivot node is max connected in cand from done or cand
+		if maxconndone > maxconn:
+			pivotnbrs = pivotdonenbrs
+		# save search status for later backout
+		stack.append( (cand, done, smallcand) )
+		cand=new_cand
+		done=new_done
+		smallcand = cand - pivotnbrs
+
+
+def compute_mailNumber(node,receivedMails,sentMails,totalMail):
+		totalMail[node] = receivedMails[node] + sentMails[node]
+
+def compute_cliqueNumber(liste, node):
+	cpt = 0
+	for clique in liste :
+		if node in clique:
+			cpt = cpt+1
+	return cpt
+
+def compute_RawCliqueScore(liste, node) :
+	rawCliqueScore = 0
+	for clique in liste:
+		if node in clique:
+			rawCliqueScore = 2 * math.exp(len(clique)-1) + rawCliqueScore
+	return rawCliqueScore
+
+def compute_weightedCliqueScore(liste, node, time) :
+	weightedCliqueScore = 0
+	for clique in liste:
+		if node in clique:
+			weightedCliqueScore = time * 2 * math.exp(len(clique)-1) + weightedCliqueScore
+	return weightedCliqueScore
+
+# Normalize the metric on mapped to a [0, 100] scale
+def NormalizeMetricValue(graph, metric) :
+	min = metric.getNodeValue(graph.getOneNode())
+	max = metric.getNodeValue(graph.getOneNode())
+		
+	for n in graph.getNodes():
+		valeur = metric.getNodeValue(n)
+		if valeur > max:
+			max = valeur
+		if valeur < min:
+			min = valeur	
+
+	for n in graph.getNodes():
+		normalizedValue = (100 * ((metric.getNodeValue(n) - min) / (max - min)))
+		metric.setNodeValue(n, normalizedValue)
+
+def compute_SocialScore(graph, metrics, storageProperty) :
+	for n in graph.getNodes():
+		totalWeightedContribution = 0	
+		totalWeigth = 0	
+		for metric,weight in metrics.items():
+			totalWeightedContribution = totalWeightedContribution + metric.getNodeValue(n) * weight
+			totalWeigth = totalWeigth + weight		
+		score = totalWeightedContribution/totalWeigth
+		storageProperty.setNodeValue(n,score)
 
 def main(graph): 
 
@@ -307,30 +348,69 @@ def main(graph):
 	#Properties of the graph
 	nodeName = graph.getStringProperty("nodeName")
 	nodeDegree =  graph.getDoubleProperty("nodeDegree")
-	nodeCentrality =  graph.getDoubleProperty("nodeCentrality")
-	cliqueNumber =  graph.getIntegerProperty("cliqueNumber")	
+	betweenessCentrality =  graph.getDoubleProperty("betweenessCentrality")
+	cliqueNumber =  graph.getDoubleProperty("cliqueNumber")	
 	rawUserCliqueScore = graph.getDoubleProperty("rawUserCliqueScore")
-	weightedUserCliqueScore = graph.getDoubleProperty("weightedUserCliqueScore")
 	clusteringCoefficient = graph.getDoubleProperty("clusteringCoefficient")
 	shortestPath = graph.getDoubleProperty("shortestPath")
-	socialScore = graph.getDoubleProperty("socialScore")
 	auth = graph.getDoubleProperty("auth")
 	hub = graph.getDoubleProperty("hub")
 	color = graph.getColorProperty("viewColor")
-	receivedMails = graph.getIntegerProperty("received_mails")
-	sentMails = graph.getIntegerProperty("sent_mails")
-	totalMail = graph.getIntegerProperty("totalMail")
+	receivedMails = graph.getDoubleProperty("received_mails")
+	sentMails = graph.getDoubleProperty("sent_mails")
+	totalMail = graph.getDoubleProperty("totalMail")
 	avgResponseTime = graph.getDoubleProperty("response_time")
+	weightedCliqueScore = graph.getDoubleProperty("weightedCliqueScore")
+	socialScore = graph.getDoubleProperty("socialScore")
 
 	# Email Corpus parsing   
    #enronpath = "/net/cremi/cbadiola/travail/bioInfo/enron_mail_20110402/maildirtest/"
-	enronpath = "C:/Users/samuel/Desktop/ENRON/enron_mail_20110402/maildir2/"
+	enronpath = "C:/Users/samuel/Desktop/ENRON/enron_mail_20110402/maildir/"
 	myParser = mailParser(graph, enronpath)
 	myParser.parse()
 	
-	# Computation of Metrics
-	compute_mailNumber(graph,receivedMails,sentMails,totalMail)
+	# Structural indicators
+	liste = list(find_cliques(graph))
+	for n in graph.getNodes():	
+		compute_mailNumber(n,receivedMails,sentMails,totalMail)
+		nbOccurence = compute_cliqueNumber(liste,n)
+		cliqueNumber.setNodeValue(n,nbOccurence)
+		CliqueScore = compute_RawCliqueScore(liste,n)
+		rawUserCliqueScore.setNodeValue(n, CliqueScore)
+		weightedScore = compute_weightedCliqueScore(liste,n,avgResponseTime.getNodeValue(n))
+		weightedCliqueScore.setNodeValue(n,weightedScore)
+		
+	# Degree centrality (nodeDegree)
+	graph.computeDoubleProperty("Degree", nodeDegree)
 
+	# Clustering coefficient (clusteringCoefficient)
+	tlp.clusteringCoefficient(graph,clusteringCoefficient,graph.numberOfNodes())
 
+	# Mean of shortest path lenght (shortestPath)
+	dataSet = tlp.DataSet()
+	dataSet["closeness centrality"] = True
+	dataSet["norm"] = False
+	graph.computeDoubleProperty("Eccentricity", shortestPath, dataSet)
+	
+	# Betweeness centrality (betweenessCentrality)
+	graph.computeDoubleProperty("Betweenness Centrality", betweenessCentrality)
+	
+	# Hub and Authorities importance
+	hits(graph, auth, hub)
 
-	#hits(graph, auth, hub)
+	# Normalisation des indicateurs sur une echelle [0-100]
+	NormalizeMetricValue(graph,totalMail)
+	NormalizeMetricValue(graph,avgResponseTime)
+	NormalizeMetricValue(graph,cliqueNumber)
+	NormalizeMetricValue(graph,rawUserCliqueScore)
+	NormalizeMetricValue(graph,weightedCliqueScore)
+	NormalizeMetricValue(graph,nodeDegree)
+	NormalizeMetricValue(graph,clusteringCoefficient)
+	NormalizeMetricValue(graph,shortestPath)
+	NormalizeMetricValue(graph,betweenessCentrality)
+	NormalizeMetricValue(graph,hub)
+	NormalizeMetricValue(graph,auth)
+	
+	# List of metrics with their weight to compute the social score
+	metrics = {totalMail:1, avgResponseTime:1, cliqueNumber:1, rawUserCliqueScore:1,weightedCliqueScore:1, nodeDegree:1, clusteringCoefficient:1,shortestPath:1, betweenessCentrality:1, hub:1, auth:1}
+	compute_SocialScore(graph,metrics,socialScore)
